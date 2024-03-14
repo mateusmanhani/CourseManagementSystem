1a275b8177f44486968d9e0a28cce555030c1e25
Mateus Manhani <58527872+mateusmanhani@users.noreply.github.com>
2024-03-14 22:03:03 +0000

    implemented user class with role attribute and strategies

---
+++ src/coursemanagementsystem/Role.java
@@ -0,0 +1,13 @@
+
+package coursemanagementsystem;
+
+/**
+ *
+ * @author Mateus Manhani
+ */
+public enum Role {
+    ADMIN,
+    OFFICE,
+    LECTURER
+    
+}
--- src/coursemanagementsystem/User.java
+++ src/coursemanagementsystem/User.java
@@ -1,13 +1,64 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package coursemanagementsystem;
 
 /**
  *
- * @author Mateus
+ * @author Mateus Manhani
  */
 public class User {
+    private String username;
+    private String password;
+    private Role role;
+    
+    // STRATEGY iNTERFACES
+    private ReportGenerationStrategy reportStrategy;
+    private UserManagementStrategy userManagementStrategy;
+
+    public User(String username, String password, Role role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public ReportGenerationStrategy getReportStrategy() {
+        return reportStrategy;
+    }
+
+    public UserManagementStrategy getUserManagementStrategy() {
+        return userManagementStrategy;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public void setReportStrategy(ReportGenerationStrategy reportStrategy) {
+        this.reportStrategy = reportStrategy;
+    }
+
+    public void setUserManagementStrategy(UserManagementStrategy userManagementStrategy) {
+        this.userManagementStrategy = userManagementStrategy;
+    }
+    
     
 }
