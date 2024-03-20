e3ad5cc3b60213da6e0f150cd53680a6ccf0b2ac
Mateus Manhani <58527872+mateusmanhani@users.noreply.github.com>
2024-03-20 22:16:38 +0000

    Instanciated andAuthService object in the menu class
    in this menu class we will actually do the user login

--- src/coursemanagementsystem/MenuSystem.java
+++ src/coursemanagementsystem/MenuSystem.java
@@ -5,5 +5,9 @@ package coursemanagementsystem;
  * @author Mateus Manhani
  */
 public class MenuSystem {
-
+    private AuthService authService;
+    
+    public MenuSystem(DatabaseIO databaseIO){
+        this.authService = new AuthService(databaseIO);
+    }
 }
