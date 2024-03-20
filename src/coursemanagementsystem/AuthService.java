2a1018cbb9a960e4716db6f7a4ee5c0bcdf0cab8
Mateus Manhani <58527872+mateusmanhani@users.noreply.github.com>
2024-03-20 22:14:09 +0000

    implemented authenticateUser method to run the username and password on the database and retrieve the user login in

--- src/coursemanagementsystem/AuthService.java
+++ src/coursemanagementsystem/AuthService.java
@@ -1,6 +1,11 @@
 
 package coursemanagementsystem;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author Mateus
@@ -12,5 +17,26 @@ public class AuthService {
         this.databaseIO = databaseIO;
     }
     
-    
+    public User authenticateUser(String username, String password) throws SQLException{
+        String query = "SELECT user_id, username, rolw, lecturer_id FROM users WHERE username = ? AND password = ?";
+        
+        try(Connection conn = databaseIO.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(query)){
+            
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            try(ResultSet rs = stmt.executeQuery()){
+                if(rs.next()){
+                    String userId = rs.getString("user_id");
+                    String roleStr = rs.getString("role");
+                    Role role = Role.valueOf(roleStr.toUpperCase());
+                    String lecturerId = rs.getString("lecture_id");
+                    return new User(userId, username, role, lecturerId);
+                }
+            }catch(SQLException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
 }
