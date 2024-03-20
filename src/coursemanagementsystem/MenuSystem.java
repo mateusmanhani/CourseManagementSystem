09240ddd796b70505f3b830f1d702dc683385d89
Mateus Manhani <58527872+mateusmanhani@users.noreply.github.com>
2024-03-20 22:21:54 +0000

    implementing login in menuclass
    still need to check for more errors and insert code into while loop

--- src/coursemanagementsystem/MenuSystem.java
+++ src/coursemanagementsystem/MenuSystem.java
@@ -1,5 +1,10 @@
 package coursemanagementsystem;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Mateus Manhani
@@ -9,5 +14,32 @@ public class MenuSystem {
     
     public MenuSystem(DatabaseIO databaseIO){
         this.authService = new AuthService(databaseIO);
+    }
+    
+    public void showLoginScreen(){
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Welcome to the Course Management System (CMS)");
+            
+            System.out.println("Username: ");
+            String username = sc.nextLine();
+            
+            System.out.println("Password: ");
+            String password = sc.nextLine();
+            
+            User user = authService.authenticateUser(username, password);
+            
+            if(user != null){
+                System.out.println("Login Successful.");
+                // continue with operations...
+            }else{
+                System.out.println("Login failed. Please check your username and password.");
+                // do a loop to keep going
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(MenuSystem.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
     }
 }
