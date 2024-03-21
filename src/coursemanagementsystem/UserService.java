f1286a7625c0421a69b4b4bac1e80c161cd7e045
Mateus Manhani <58527872+mateusmanhani@users.noreply.github.com>
2024-03-21 09:48:59 +0000

    implemented delete user method
    takes userId as parameter gets a connection and delete user if any rows are affected true is returned. handle exception and provide error message

--- src/coursemanagementsystem/UserService.java
+++ src/coursemanagementsystem/UserService.java
@@ -1,4 +1,3 @@
-
 package coursemanagementsystem;
 
 import java.sql.Connection;
@@ -10,29 +9,47 @@ import java.sql.SQLException;
  * @author Mateus Manhani
  */
 public class UserService {
+
     private DatabaseIO databaseIO;
-    
-    public UserService(DatabaseIO databaseIO){
+
+    public UserService(DatabaseIO databaseIO) {
         this.databaseIO = databaseIO;
     }
-    
-    public boolean addUSer(User user){
-        String query = " INSERT INTO users (username, password, role, lecturer_id) VALUES ( ?, ?, ?, ?);";
-        try(Connection conn = databaseIO.getConnection();
-            PreparedStatement stmt = conn.prepareStatement(query)){
-            
-            stmt.setString(1, user.getUsername());
-            stmt.setString(2, user.getPassword());
-            stmt.setString(3, user.getRole().toString());
-            stmt.setString(4,user.getLecturerId());
-            
+
+    public boolean addUSer(User user) {
+        String query = " INSERT INTO users (user_id, username, password, role, lecturer_id) VALUES ( ?, ?, ?, ?);";
+        try ( Connection conn = databaseIO.getConnection();  PreparedStatement stmt = conn.prepareStatement(query)) {
+
+            stmt.setString(1, user.getUserID());
+            stmt.setString(2, user.getUsername());
+            stmt.setString(3, user.getPassword());
+            stmt.setString(4, user.getRole().name());
+            stmt.setString(5, user.getRole() == Role.LECTURER ? user.getLecturerId() : null); // if user role is LECTURER also insert lecturerId else insert null
+
             int rowsAffected = stmt.executeUpdate();
             return rowsAffected > 0; // If any rows were actually updated it ill return true;
-            
-        }catch(SQLException e){
+
+        } catch (SQLException e) {
             System.out.println("Error adding user.");
             e.getSQLState();
-            return false;   
+            return false;
         }
     }
+
+    public boolean deleteUser(String userId) {
+        String query = "DELETE FROM users WHERE user_id = ?";
+        try ( Connection conn = databaseIO.getConnection();  PreparedStatement stmt = conn.prepareStatement(query)) {
+
+            stmt.setString(1, userId);
+            int rowsAffected = stmt.executeUpdate();
+            return rowsAffected > 0; //return true if any rows are affected.
+
+        } catch (SQLException e) {
+            System.out.println("Error deleting user.");
+            e.getSQLState();
+            return false;
+        }
+    }
+    
+    
 }
