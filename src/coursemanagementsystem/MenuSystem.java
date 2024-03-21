ed54940336402bd174e1f2fe32d8188c5ccd0173
Mateus Manhani <58527872+mateusmanhani@users.noreply.github.com>
2024-03-21 09:16:19 +0000

    defined what methods and logic I will follow in the menuSystem class
    No implementation yet, moving on to a userService class to interact with the database and update all operation with users

--- src/coursemanagementsystem/MenuSystem.java
+++ src/coursemanagementsystem/MenuSystem.java
@@ -17,29 +17,80 @@ public class MenuSystem {
     }
     
     public void showLoginScreen(){
-        try {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Welcome to the Course Management System (CMS)");
-            
-            System.out.println("Username: ");
-            String username = sc.nextLine();
-            
-            System.out.println("Password: ");
-            String password = sc.nextLine();
-            
-            User user = authService.authenticateUser(username, password);
-            
-            if(user != null){
-                System.out.println("Login Successful.");
-                // continue with operations...
-            }else{
-                System.out.println("Login failed. Please check your username and password.");
-                // do a loop to keep going
+        boolean validInput = false;
+        while(!validInput){
+            try {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Welcome to the Course Management System (CMS)");
+
+                System.out.println("Username: ");
+                String username = sc.nextLine();
+
+                System.out.println("Password: ");
+                String password = sc.nextLine();
+
+                User user = authService.authenticateUser(username, password);
+
+                if(user != null){
+                    System.out.println("Login Successful.");
+                    // continue with operations...
+                }else{
+                    System.out.println("Login failed. Please check your username and password.");
+                    // do a loop to keep going
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(MenuSystem.class.getName()).log(Level.SEVERE, null, ex);
             }
-        } catch (SQLException ex) {
-            Logger.getLogger(MenuSystem.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+    }
+    
+    public void showUserMenu(User user){
+        Role userRole = user.getRole();
+        
+        switch(userRole){
+            case OFFICE:
+                // SHOW OFFICE MENU
+            case LECTURER:
+                //SHOW LECTURER MENU
+            case ADMIN:
+                //SHOW ADMIN MENU
         }
         
+    }
+    
+    public void adminMenu(){
+        //addUser
         
+        //updateUser
+            //role
+            //username
+            //password
+        
+        //deleteUser
+        
+        // change my username
+        
+        //change my password
+    }
+    
+    public void lecturerMenu(){
+        //generate my report
+        
+        // change my password
+        
+        //change my username
+    }
+    
+    public void officeMenu(){
+        //generate student report
+        
+        //generate lecturer report
+        
+        //generate course report
+        
+        //change my password
+        
+        //change my username
     }
 }
